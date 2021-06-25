456eb2a28e0cdd47851159c08eabdeae963228e6
JsonCC <13233768245@163.com>
2021-06-25 21:14:45 +0800

    分页

--- src/views/oil/oil.vue
+++ src/views/oil/oil.vue
@@ -87,20 +87,33 @@
         </template>
       </el-table-column>
 
-      <el-table-column label="操作">
+      <el-table-column label="操作" width="200">
         <template v-slot="scope">
           <font-awesome-icon v-bind:icon="['fas','play-circle']" size="lg" title="启用" class="fasbtn" @click="chgstauts(scope.row.id,1)" v-if="scope.row.status==0"></font-awesome-icon>
           <font-awesome-icon v-bind:icon="['fas','pause-circle']" size="lg" title="禁用" class="fasbtn" @click="chgstauts(scope.row.id,0)" v-else></font-awesome-icon>
-          <font-awesome-icon v-bind:icon="['fas','edit']" size="lg" title="编辑" class="fasbtn" @click="saveorupdate(scope.row)"></font-awesome-icon>&nbsp&nbsp
+          <font-awesome-icon v-bind:icon="['fas','edit']" size="lg" title="编辑" class="fasbtn" @click="saveorupdate(scope.row)"></font-awesome-icon>
           <font-awesome-icon v-bind:icon="['fas','trash-alt']" size="lg" title="删除" class="fasbtn" @click="del(scope.row.id)"></font-awesome-icon>
+          <font-awesome-icon v-bind:icon="['fas','yen-sign']" size="lg" title="充值记录" class="fasbtn" @click="topup(scope.row)"></font-awesome-icon>
         </template>
       </el-table-column>
     </el-table>
-    <!-- 新增或修改 -->
-    <!--添加或修改-->
-    <!-- <oil-save-or-update :visible.sync="isshow" @refeshData="queryOil" :tmpoil="showdata"></oil-save-or-update> -->
-    <oil-save-or-update ref="saveOrUpdate" :visible.sync="isshow" @refreshData="queryOil" :tmpoil="showdata"></oil-save-or-update>
-  </div>
+
+  <!-- 分页 -->
+  <el-row class="fenye">
+        共{{total}}条
+        <span class="myxs">每页显示</span>
+        <el-select size="small" class="sel" v-model="pageSize" v-on:change="chg">
+          <el-option v-for="(p,index) in pageoptions" v-bind:value="p" :key="index">{{p}}</el-option>
+        </el-select><span style="margin-left: 5px; margin-right: 20px;">条</span>
+        <el-pagination background layout="prev, pager, next" :total="total" :page-size="pageSize"  @current-change="goto">
+        </el-pagination>
+      </el-row>
+      <!-- 新增或修改 -->
+        <!--添加或修改-->
+        <!-- <oil-save-or-update :visible.sync="isshow" @refeshData="queryOil" :tmpoil="showdata"></oil-save-or-update> -->
+        <oil-save-or-update ref="saveOrUpdate" :visible.sync="isshow" @refreshData="queryOil" :tmpoil="showdata"></oil-save-or-update>
+      </div>
+
 </template>
 
 <script>
@@ -121,11 +134,16 @@
           status: ''
         },
 
-        //在这里定义数据属性
+        //在这里定义数据属性（有分页的）
         oils: [],
         isshow: false, //是否显示添加框
         host: host + "/",
-        showdata: {}
+        showdata: {},
+        pageoptions: [5, 10, 15, 20, 50, 100],
+        page: 1, //默认第一页数据
+        pageSize: 10, //默认一页显示10条记录
+        total: 0, //总记录数
+        pageTotal: 1 //总页数
       }
     },
     components: {
@@ -133,6 +151,26 @@
     },
 
     methods: {
+      //分页
+      // 更改每页显示的记录数
+      chg: function() {
+        this.page = 1; //当每页记录数变化 时   查询 第一页记录
+        this.queryOil();
+      },
+      //页面跳转
+      goto(page){
+        //更改当前页数
+        this.page=page;
+        this.queryOil();
+      },
+
+      handleSizeChange(val) {
+        console.log(`每页 ${val} 条`);
+      },
+      handleCurrentChange(val) {
+        console.log(`当前页: ${val}`);
+      },
+
       //启用和禁用
       async chgstauts(id,status){
         let params = {
@@ -160,6 +198,17 @@
           if (response.code == 200) {
             // 获取数据
             this.oils = response.data;
+            //分页
+            //当前页数
+            this.page = response.page;
+            //每页记录数
+            this.pageSize = parseInt(response.pageSize);
+            //总记录数
+            this.total = parseInt(response.total);
+
+            var tmp = parseInt(this.total / this.pageSize);
+            //总页数
+            this.pageTotal = this.total % this.pageSize == 0 ? tmp : (tmp + 1);
 
           } else {
             this.$message({
@@ -243,4 +292,21 @@
     align-items: center;
     margin-bottom: 20px;
   }
+
+  //分页
+  .fenye {
+      text-align: right;
+      margin-top: 20px;
+      display: -webkit-flex;
+      display: flex;
+      justify-content: flex-end;
+      align-items: center;
+    }
+    .myxs {
+      margin-left: 20px;
+      margin-right: 5px;
+    }
+    .sel {
+      width: 100px;
+    }
 </style>
